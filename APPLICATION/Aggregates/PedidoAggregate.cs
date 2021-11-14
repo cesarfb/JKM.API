a01bd94de6c0fd563254cf5183cfdc84f0b04ac6
Mauricio06Everis <mauricioandre.huacchapacheco@nttdata.everis.com>
2021-11-14 11:10:24 -0500

    Cambios por mejorar para pedido

--- APPLICATION/Aggregates/PedidoAggregate.cs
+++ APPLICATION/Aggregates/PedidoAggregate.cs
@@ -8,26 +8,26 @@ namespace JKM.APPLICATION.Aggregates
     public class PedidoModelPaginado
     {
         public int IdPedido { get; set; }
-        private DateTime FechaRegistro { get; set; }
-        [JsonProperty("fechaRegistro")]
-        public string FechaRegistroString
+        public DateTime FechaRegistro { get; set; }
+        [JsonProperty("fechaRegistroString")]
+        public string fechaRegistroString
         {
             get
             {
                 return FechaRegistro.ToString("yyyy-MM-dd");
             }
         }
-        private DateTime FechaEntrega { get; set; }
-        [JsonProperty("fechaEntrega")]
-        public string FechaEntregaString
+        public DateTime FechaEntrega { get; set; }
+        [JsonProperty("fechaEntregaString")]
+        public string fechaEntregaString
         {
             get
             {
                 return FechaEntrega.ToString("yyyy-MM-dd");
             }
         }
-        private int IdEstado { get; set; }
-        private string Descripcion { get; set; }
+        public int IdEstado { get; set; }
+        public string Descripcion { get; set; }
         public Identifier Estado
         {
             get
@@ -47,16 +47,19 @@ namespace JKM.APPLICATION.Aggregates
         public string Email { get; set; }
         public int Cantidad { get; set; }
         public float PrecioProd { get; set; }
+        public float PrecioTotal { get; set; }
         public int IdProducto { get; set; }
         public string Codigo { get; set; }
         public string Nombre { get; set; }
         public string Imagen { get; set; }
+        public string Cliente { get; set; }
     }
     public class PedidoModelFormat : PedidoModelPaginado
     {
         public int Precio { get; set; }
         public string Solicitante { get; set; }
         public string Email { get; set; }
+        public string Cliente { get; set; }
         public List<Pedidos> Pedidos { get; set; }
     }
 
--- APPLICATION/Queries/Pedido/GetPedido/GetPedidoQueryHandler.cs
+++ APPLICATION/Queries/Pedido/GetPedido/GetPedidoQueryHandler.cs
@@ -3,6 +3,7 @@ using JKM.APPLICATION.Aggregates;
 using JKM.UTILITY.Utils;
 using MediatR;
 using System;
+using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 using System.Threading;
@@ -10,7 +11,7 @@ using System.Threading.Tasks;
 
 namespace JKM.APPLICATION.Queries.Pedido.GetPedido
 {
-    public class GetPedidoQueryHandler : IRequestHandler<GetPedidoQuery, PaginadoResponse<PedidoModelPaginado>>
+    public class GetPedidoQueryHandler : IRequestHandler<GetPedidoQuery, IEnumerable<PedidoModel>>
     {
         private readonly IDbConnection _conexion;
         public GetPedidoQueryHandler(IDbConnection conexion)
@@ -18,36 +19,45 @@ namespace JKM.APPLICATION.Queries.Pedido.GetPedido
             _conexion = conexion;
         }
 
-        public async Task<PaginadoResponse<PedidoModelPaginado>> Handle(GetPedidoQuery request, CancellationToken cancellationToken)
+        public async Task<IEnumerable<PedidoModel>> Handle(GetPedidoQuery request, CancellationToken cancellationToken)
         {
-            string sql = $@"SELECT COUNT(1) FROM Pedido;";
+             //sql = $@"SELECT COUNT(1) FROM Pedido;";
 
-            sql += $@"SELECT P.idPedido, P.fechaRegistro, P.fechaEntrega, 
+            string sql = $@"SELECT P.idPedido, P.fechaRegistro, P.fechaEntrega, 
 					  EP.idEstado, EP.descripcion,
-					  V.precio, C.solicitante, C.email
+					  V.precio, C.solicitante, C.email,
+                      CLI.razonSocial as 'cliente'
 					  FROM Pedido P
                       INNER JOIN EstadoPedido EP ON EP.idEstado = P.idEstado
                       INNER JOIN Venta V ON V.idVenta = P.idVenta
                       INNER JOIN Cotizacion C ON C.idCotizacion = V.idCotizacion
+                      INNER JOIN Cliente CLI ON C.idCliente = CLI.idCliente
 					  ORDER BY P.idPedido DESC;";
 
             using (IDbConnection connection = _conexion)
             {
                 try
                 {
-                    PaginadoResponse<PedidoModelPaginado> newPaginado = new PaginadoResponse<PedidoModelPaginado>();
+                    //PaginadoResponse<PedidoModelPaginado> newPaginado = new PaginadoResponse<PedidoModelPaginado>();
 
+                    //connection.Open();
+                    //using (var multi = await connection.QueryMultipleAsync(sql))
+                    //{
+                    //    newPaginado.TotalRows = multi.ReadFirst<int>();
+                    //    newPaginado.Data = multi.Read<PedidoModel>().AsList();
+                    //}
+                    //connection.Close();
+
+                    //if (newPaginado.Data.AsList().Count == 0) throw new ArgumentNullException();
+
+                    //return newPaginado;
                     connection.Open();
-                    using (var multi = await connection.QueryMultipleAsync(sql))
-                    {
-                        newPaginado.TotalRows = multi.ReadFirst<int>();
-                        newPaginado.Data = multi.Read<PedidoModel>().AsList();
-                    }
+                    IEnumerable<PedidoModel> pedidos =
+                            await connection.QueryAsync<PedidoModel>(sql);
                     connection.Close();
 
-                    if (newPaginado.Data.AsList().Count == 0) throw new ArgumentNullException();
-
-                    return newPaginado;
+                    if (pedidos.AsList().Count == 0) throw new ArgumentNullException();
+                    return pedidos;
                 }
                 catch (SqlException err)
                 {
